6aee9b48dfe16e4fc1457af09fa5aed62c27dfc0
Alex Kong <alex.lt.kong@connect.ust.hk>
2022-04-09 11:07:16 +0800

    housekeeping

--- README.md
+++ README.md
@@ -1,4 +1,7 @@
-# Image Classification Pipeline Component
+# Tensorflow Pipeline
 
-A convolutional neural network-based image classification program built to be
-a component in a pipeline of data processing.
+An framework built to be a component in a pipeline of data processing using `Tensorflow`.
+
+Note that this repo is a pipeline framework only and the model construction
+itself is *not* in it: model will be dynamically loaded to the pipeline
+according to the path defined in the `config.json` file.
--- train.py
+++ train.py
@@ -1,12 +1,11 @@
 from tensorflow import keras
-from tensorflow.keras import layers
 
 import utils
 import logging
-import matplotlib.pyplot as plt
 import os
 import pandas as pd
 import shutil
+import sys
 import tensorflow as tf
 
 
@@ -21,68 +20,6 @@ def preview_samples(dest_dir, dataset):
       tf.keras.utils.save_img(
         os.path.join(label_dir, f'{i}.jpg'), images[i].numpy().astype("uint8")
       )
-
-
-def make_model(input_shape, num_classes, dropout=0.5):
-
-    data_augmentation = keras.Sequential(
-      [
-        layers.RandomFlip("horizontal"),
-        layers.RandomRotation(0.1)
-      ]
-    )
-
-    inputs = keras.Input(shape=input_shape)
-    # Image augmentation block
-    x = data_augmentation(inputs)
-
-    # Entry block
-    x = layers.Rescaling(1.0 / 255)(x) # Shrink RGB channel values from [0, 255] range to [0, 1] range.
-    x = layers.Conv2D(32, 3, strides=2, padding="same")(x)
-    x = layers.BatchNormalization()(x)
-    x = layers.Activation("relu")(x)
-
-    x = layers.Conv2D(64, 3, padding="same")(x)
-    x = layers.BatchNormalization()(x)
-    x = layers.Activation("relu")(x)
-    
-    previous_block_activation = x  # Set aside residual
-
-    for size in [128, 256, 512, 728]:
-      x = layers.Activation("relu")(x)      
-      x = layers.SeparableConv2D(size, 3, padding="same")(x)
-      x = layers.BatchNormalization()(x)
-
-      x = layers.Activation("relu")(x)      
-      x = layers.SeparableConv2D(size, 3, padding="same")(x)
-      x = layers.BatchNormalization()(x)
-
-      x = layers.MaxPooling2D(3, strides=2, padding="same")(x)
-      x = layers.Dropout(dropout)(x)
-      # Dropout added per https://machinelearningmastery.com/how-to-reduce-overfitting-with-dropout-regularization-in-keras/
-
-      # Project residual
-      residual = layers.Conv2D(size, 1, strides=2, padding="same")(
-          previous_block_activation
-      )
-      x = layers.add([x, residual])  # Add back residual
-      previous_block_activation = x  # Set aside next residual
-
-    x = layers.SeparableConv2D(1024, 3, padding="same")(x)
-    x = layers.BatchNormalization()(x)
-    x = layers.Activation("relu")(x)
-
-    x = layers.GlobalAveragePooling2D()(x)
-    if num_classes == 2:
-        activation = "sigmoid"
-        units = 1
-    else:
-        activation = "softmax"
-        units = num_classes
-
-    x = layers.Dropout(dropout)(x) # Original dropout layer in Keras example.
-    outputs = layers.Dense(units, activation=activation)(x)
-    return keras.Model(inputs, outputs)
 
 
 def main():
@@ -104,7 +41,9 @@ def main():
   train_ds = train_ds.prefetch(buffer_size=32)
   val_ds = val_ds.prefetch(buffer_size=32)
 
-  model = make_model(input_shape=image_size + (3,), num_classes=2, dropout=settings['model']['dropout'])
+  sys.path.insert(1, settings['model']['path'])
+  import definition
+  model = definition.make_model(input_shape=image_size + (3,), num_classes=2, dropout=settings['model']['dropout'])
   keras.utils.plot_model(
     model,
     show_shapes=True,
