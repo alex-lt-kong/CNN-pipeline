7bbb68bf3876cb0af320eaee46a6cf259ab93c27
Alex Kong <alex.lt.kong@connect.ust.hk>
2022-04-17 02:20:54 +0800

    minor update

--- predict.py
+++ predict.py
@@ -65,10 +65,10 @@ def generate_final_prediction(predictions):
   predictions.sort(reverse=True) 
   if len(predictions) == 0:
     print('Error')
-    return
+    return None
   score = sum(predictions)/len(predictions)
   print(f'{predictions[0:5]}, ({100 * score:.2f}%)')
-  
+  return score
 
 def main():
 
@@ -78,7 +78,7 @@ def main():
 
   listing = glob.glob(settings['prediction']['source'])
   listing.sort(reverse=True) 
-  for file_path in listing[10:]:
+  for file_path in listing[1:]:
     if os.path.isfile(file_path):
       videos.append(file_path)
 
@@ -90,21 +90,23 @@ def main():
     prediction_records = {}
     print(prediction_records)
   model = tf.keras.models.load_model(settings['model']['save_to']['model'])
-  for video in videos:
-    if video in prediction_records:
-      print(f'{video} in predictions')
+  for video_path in videos:
+    video_name = os.path.basename(video_path)
+    if video_name in prediction_records:
+      print(f'{video_name} in predictions')
     else:
-      print(f'{video} NOT in predictions', end='')
-      video_to_frames(video, settings["prediction"]["frames_dir"])
+      print(f'{video_name} NOT in predictions', end='')
+      video_to_frames(video_path, settings["prediction"]["frames_dir"])
       predictions = predict_frames(
         model,
         settings["prediction"]["frames_dir"] + '*.jpg',
         (settings['dataset']['image']['height'], settings['dataset']['image']['width'])
       )
       score = generate_final_prediction(predictions)
-      prediction_records[video] = score
+      if score is not None:
+        prediction_records[video_name] = score
       with open(settings['prediction']['results'], 'w') as fp:
-        json.dump(prediction_records, fp, ensure_ascii=False)
+        json.dump(prediction_records, fp, ensure_ascii=False, indent=2)
 
 
 
