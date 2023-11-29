7b633ee9d731306736dc06b2206643cda6db8d0b
Alex Kong <akong@y-intercept.net>
2023-11-29 14:56:18 +0800

    minor update

--- src/cpp/utils.h
+++ src/cpp/utils.h
@@ -1,19 +1,21 @@
 #ifndef VBCP_UTILS_H
 #define VBCP_UTILS_H
 
-#include <deque>
-#include <mutex>
-#include <signal.h>
-
 #include <nlohmann/json.hpp>
 #pragma GCC diagnostic push
 #pragma GCC diagnostic ignored "-Wunused-parameter"
 #include <torch/script.h> // One-stop header.
 #pragma GCC diagnostic pop
 
+#include <deque>
+#include <iostream>
+#include <mutex>
+#include <signal.h>
 template <class T> class PercentileTracker {
 public:
-  PercentileTracker(size_t sample_size) : sample_size(sample_size) {}
+  PercentileTracker(size_t sample_size) : sample_size(sample_size) {
+    refreshStatsCalled = false;
+  }
 
   void addNumber(T num) {
     data.push_back(num);
@@ -27,6 +29,10 @@ public:
   double getPercentile(double percent) {
     if (data.empty())
       return -1;
+    if (!refreshStatsCalled) {
+      std::cerr << "WARNING: refreshStats() not called before calling "
+                   "getPercentile(), the result will be nonsense!\n";
+    }
 
     int index = percent / 100.0 * data.size() - 1;
     return data[index];
@@ -37,6 +43,7 @@ public:
 private:
   std::deque<T> data;
   size_t sample_size;
+  bool refreshStatsCalled;
 };
 
 static void signal_handler(int signum);
