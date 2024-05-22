2808052ef28a0999b20ed2c44a11127f55472099
Alex Kong <alex.lt.kong@connect.ust.hk>
2024-05-22 20:35:20 +0800

    new models

--- src/modeling/model_driver.py
+++ src/modeling/model_driver.py
@@ -1,6 +1,6 @@
 from model_resnet import resnet18, resnet34, resnet50
 from model_mobilenetv3 import mobilenet_v3_small
-from model_vggmm import vggmm, vgg3m, vgg4m, vgg5m, vgg6m, vgg7m, vgg8m
+from model_vgg import vgg2m, vgg3m, vgg4m, vgg5m, vgg6m, vgg7m, vgg8m
 from model_squeezenet import squeezenet1_1
 
 from sklearn import metrics
--- src/modeling/model_vgg.py
+++ src/modeling/model_vgg.py
@@ -3,110 +3,108 @@ from typing import Any, Dict, Tuple
 import torch
 import torch.nn as nn
 
+# Original VGG16 implementation from here: https://blog.paperspace.com/vgg-from-scratch-pytorch/
 
 
-class VGG16MinusMinus(nn.Module):
+class VGG16TwoMinuses(nn.Module):
     dropout = 0.75
     num_classes = -1
 
-    def __init__(self, num_classes: int, target_image_size: Tuple[int, int], dropout_rate: float) -> None:
+    def __init__(
+        self, num_classes: int, target_image_size: Tuple[int, int],
+        dropout_rate: float
+    ) -> None:
         self.num_classes = num_classes
         self.dropout = dropout_rate
         self.target_image_size = target_image_size
 
-        super(VGG16MinusMinus, self).__init__()
-        # self.layer1 = nn.Sequential(
-        #     nn.Conv2d(3, 64, kernel_size=3, stride=1, padding=1),
-        #     nn.BatchNorm2d(64),
-        #     nn.ReLU())
-        self.layer2 = nn.Sequential(
-            nn.Conv2d(3, 8, kernel_size=3, stride=1, padding=1),
-            nn.BatchNorm2d(8),
-            nn.ReLU(),
-            nn.MaxPool2d(kernel_size=2, stride=2))
-        self.layer3 = nn.Sequential(
-            nn.Conv2d(8, 12, kernel_size=3, stride=1, padding=1),
-            nn.BatchNorm2d(12),
-            nn.ReLU())
-        # self.layer4 = nn.Sequential(
-        #    nn.Conv2d(128, 128, kernel_size=3, stride=1, padding=1),
-        #    nn.BatchNorm2d(128),
-        #    nn.ReLU(),
-        #    nn.MaxPool2d(kernel_size=2, stride=2))
-        self.layer5 = nn.Sequential(
-            nn.Conv2d(12, 16, kernel_size=3, stride=1, padding=1),
-            nn.BatchNorm2d(16),
-            nn.ReLU(),
-            nn.MaxPool2d(kernel_size=2, stride=2))
-        # self.layer6 = nn.Sequential(
-        #    nn.Conv2d(256, 256, kernel_size=3, stride=1, padding=1),
-        #    nn.BatchNorm2d(256),
-        #    nn.ReLU())
-        self.layer7 = nn.Sequential(
-            nn.Conv2d(16, 32, kernel_size=3, stride=1, padding=1),
+        super(VGG16TwoMinuses, self).__init__()
+        self.layer1 = nn.Sequential(
+            nn.Conv2d(3, 32, kernel_size=3, stride=1, padding=1),
             nn.BatchNorm2d(32),
+            nn.ReLU())
+        self.layer2 = nn.Sequential(
+            nn.Conv2d(32, 32, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(32),
+            nn.ReLU(), 
+            nn.MaxPool2d(kernel_size = 2, stride = 2))
+        self.layer3 = nn.Sequential(
+            nn.Conv2d(32, 64, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(64),
+            nn.ReLU())
+        self.layer4 = nn.Sequential(
+            nn.Conv2d(64, 64, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(64),
             nn.ReLU(),
-            nn.MaxPool2d(kernel_size=2, stride=2))
-        # self.layer8 = nn.Sequential(
-        #     nn.Conv2d(256, 512, kernel_size=3, stride=1, padding=1),
-        #     nn.BatchNorm2d(512),
-        #     nn.ReLU())
-        # self.layer9 = nn.Sequential(
-        #    nn.Conv2d(512, 512, kernel_size=3, stride=1, padding=1),
-        #    nn.BatchNorm2d(512),
-        #    nn.ReLU())
+            nn.MaxPool2d(kernel_size = 2, stride = 2))
+        self.layer5 = nn.Sequential(
+            nn.Conv2d(64, 128, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(128),
+            nn.ReLU())
+        self.layer6 = nn.Sequential(
+            nn.Conv2d(128, 128, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(128),
+            nn.ReLU())
+        self.layer7 = nn.Sequential(
+            nn.Conv2d(128, 128, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(128),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size = 2, stride = 2))
+        self.layer8 = nn.Sequential(
+            nn.Conv2d(128, 256, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(256),
+            nn.ReLU())
+        self.layer9 = nn.Sequential(
+            nn.Conv2d(256, 256, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(256),
+            nn.ReLU())
         self.layer10 = nn.Sequential(
-            nn.Conv2d(32, 48, kernel_size=3, stride=1, padding=1),
-            nn.BatchNorm2d(48),
-            nn.ReLU(),
-            nn.MaxPool2d(kernel_size=2, stride=2))
-        # self.layer11 = nn.Sequential(
-        #     nn.Conv2d(512, 512, kernel_size=3, stride=1, padding=1),
-        #     nn.BatchNorm2d(512),
-        #     nn.ReLU())
-        self.layer12 = nn.Sequential(
-             nn.Conv2d(48, 64, kernel_size=3, stride=1, padding=1),
-             nn.BatchNorm2d(64),
-             nn.ReLU(),
-             nn.MaxPool2d(kernel_size=2, stride=2))
-        self.layer13 = nn.Sequential(
-            nn.Conv2d(64, 256, kernel_size=3, stride=1, padding=1),
+            nn.Conv2d(256, 256, kernel_size=3, stride=1, padding=1),
             nn.BatchNorm2d(256),
             nn.ReLU(),
-            nn.MaxPool2d(kernel_size=2, stride=2))
-        self.fc = nn.Sequential(
-            nn.Linear(
-                int(self.target_image_size[0] / 64) *
-                int(self.target_image_size[1] / 64) * 256,
-                int(4096 / 41)
-            ),
-            nn.Dropout(self.dropout),
+            nn.MaxPool2d(kernel_size = 2, stride = 2))
+        self.layer11 = nn.Sequential(
+            nn.Conv2d(256, 256, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(256),
             nn.ReLU())
-        self.fc1 = nn.Sequential(
-            nn.Linear(int(4096 / 41), int(4096 / 41)),
-            nn.Dropout(self.dropout),
+        self.layer12 = nn.Sequential(
+            nn.Conv2d(256, 256, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(256),
             nn.ReLU())
-        self.fc2 = nn.Sequential(
-            nn.Linear(int(4096 / 41), num_classes))
+        self.layer13 = nn.Sequential(
+            nn.Conv2d(256, 256, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(256),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size = 2, stride = 2))
+        
+        self.avgpool = nn.AdaptiveAvgPool2d((7, 7))
+        self.classifier = nn.Sequential(
+            nn.Linear(256 * 7 * 7, 256),
+            nn.ReLU(True),
+            nn.Dropout(p=self.dropout),
+            nn.Linear(256, 256),
+            nn.ReLU(True),
+            nn.Dropout(p=self.dropout),
+            nn.Linear(256, num_classes),
+        )
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
-        # x = self.layer1(x)
+        x = self.layer1(x)
         x = self.layer2(x)
         x = self.layer3(x)
-        # x = self.layer4(x)
+        x = self.layer4(x)
         x = self.layer5(x)
-        # x = self.layer6(x)
+        x = self.layer6(x)
         x = self.layer7(x)
-        # x = self.layer8(x)
-        # x = self.layer9(x)
+        x = self.layer8(x)
+        x = self.layer9(x)
         x = self.layer10(x)
-        # x = self.layer11(x)
+        x = self.layer11(x)
         x = self.layer12(x)
         x = self.layer13(x)
+        x = self.avgpool(x)
         x = x.reshape(x.size(0), -1)
-        x = self.fc(x)
-        x = self.fc1(x)
-        x = self.fc2(x)
+        x = self.classifier(x)
         return x
 
 
@@ -523,8 +521,8 @@ class VGG16EightMinuses(nn.Module):
         return x
 
 
-def vggmm(configs: Dict[str, Any], dropout_rate: float):    
-    return VGG16MinusMinus(
+def vgg2m(configs: Dict[str, Any], dropout_rate: float):    
+    return VGG16TwoMinuses(
         configs['model']['num_classes'],
         (configs['model']['input_image_size']['width'], configs['model']['input_image_size']['height']),
         dropout_rate
